729138628d8529105a5cda6295e7205e47430de7
prabhuraj <prabhurajpl66@gmail.com>
2022-12-15 17:07:21 +0530

    user-registration form *Ui & Validations

---
+++ src/app/Users/user-registration/user-registration.component.ts
@@ -0,0 +1,48 @@
+import { UsersService } from './../../Core/services/users.service';
+import { Component, OnInit } from '@angular/core';
+import { Country, State, City } from 'country-state-city';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'user-registration',
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.scss']
+})
+export class UserRegistrationComponent implements OnInit {
+
+  allCountries!: any;
+  statesBasedcountry!: any;
+  cityBasedStates!: any;
+  countryCode!: string;
+  constructor(private userservice: UsersService) { }
+  EMAIL_REGEXP = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  UserRegistrationForm = new FormGroup({
+    firstname: new FormControl('',Validators.required),
+    lastname: new FormControl(''),
+    email: new FormControl('', [ Validators.required, Validators.email, Validators.minLength(1), Validators.pattern(this.EMAIL_REGEXP)]),
+    password: new FormControl('', [Validators.required,Validators.minLength(5),]),
+    country:new FormControl('',Validators.required),
+    state: new FormControl(''),
+    city:new FormControl(''),
+  })
+
+  ngOnInit(): void {
+    this.allCountries = Country.getAllCountries()
+  }
+
+  onCountryselection(value: string) {
+    this.countryCode = value;
+    this.statesBasedcountry = State.getStatesOfCountry(value);
+  }
+  onStateselection(state: string) {
+    this.cityBasedStates = City.getCitiesOfState(this.countryCode, state);
+  }
+
+  userRegistrationSubmit(){
+
+  }
+
+
+}
